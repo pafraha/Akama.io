a8fbb3f1dbb7edae0f23c553ca02b174c7364628
R Paulin Franco <raharinirinapf@gmail.com>
2017-11-06 08:36:00 +0300

    1.0.0.8 Pagination new design with Bootstrap 4

--- src/Framework/Twig/TextExtension.php
+++ src/Framework/Twig/TextExtension.php
@@ -6,17 +6,30 @@ class TextExtension extends \Twig_Extension
     public function getFilters(): array
     {
         return [
-            new \Twig_SimpleFilter('excerpt', [$this, 'excerpt'])
+            new \Twig_SimpleFilter('excerpt', [$this, 'excerpt']),
+            new \Twig_SimpleFilter('excerpt20', [$this, 'excerpt20'])
         ];
     }
 
-    public function excerpt(string $content, int $maxLength = 100): string
+    public function excerpt(string $content, int $maxLength = 120): string
     {
         if (mb_strlen($content) > $maxLength) {
             $excerpt = mb_substr($content, 0, $maxLength);
-            $lastSpace = mb_strrpos($excerpt, ' ');
+            //$lastSpace = mb_strrpos($excerpt, ' ');
 
-            return mb_substr($excerpt, 0, $lastSpace) . '...';
+            return mb_substr($excerpt, 0, $maxLength) . '...';
+        }
+
+        return $content;
+    }
+
+    public function excerpt20(string $content, int $maxLength = 25): string
+    {
+        if (mb_strlen($content) > $maxLength) {
+            $excerpt = mb_substr($content, 0, $maxLength);
+            //$lastSpace = mb_strrpos($excerpt, ' ');
+
+            return mb_substr($excerpt, 0, $maxLength) . '...';
         }
 
         return $content;
